bda2fd3656e1d18f559e3e6a02b8506d64d47127
dominx99 <dominx992@gmail.com>
2021-03-22 01:32:02 +0100

    Change config

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 require_once './vendor/autoload.php';
 
+use App\Shared\Http\Middleware\CorsMiddleware;
 use Slim\Factory\AppFactory;
 use DI\Container;
 use App\Shared\Http\Middleware\ExceptionMiddleware;
@@ -14,21 +15,14 @@ require_once './bootstrap/dependencies.php';
 AppFactory::setContainer($container);
 $app = AppFactory::create();
 
-$app->addBodyParsingMiddleware();
 $app->addMiddleware(new ExceptionMiddleware($container->get(LoggerInterface::class)));
+$app->addMiddleware(new CorsMiddleware());
 $app->addRoutingMiddleware();
+$app->addBodyParsingMiddleware();
 $app->addErrorMiddleware(true, false, false);
 
 $app->options('/{routes:.+}', function ($request, $response, $args) {
     return $response;
-});
-
-$app->add(function ($request, $handler) {
-    $response = $handler->handle($request);
-    return $response
-            ->withHeader('Access-Control-Allow-Origin', 'https://ledsystem-landpage.herokuapp.com')
-            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 });
 
 require_once './routes/api/v1.php';
---
+++ src/Shared/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace App\Shared\Http\Middleware;
+
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Slim\Routing\RouteContext;
+
+final class CorsMiddleware implements MiddlewareInterface
+{
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $routeContext = RouteContext::fromRequest($request);
+        $routingResults = $routeContext->getRoutingResults();
+        $methods = $routingResults->getAllowedMethods();
+        $requestHeaders = $request->getHeaderLine('Access-Control-Request-Headers');
+
+        $response = $handler->handle($request);
+
+        $response = $response->withHeader('Access-Control-Allow-Origin', '*');
+        $response = $response->withHeader('Access-Control-Allow-Methods', implode(',', $methods));
+        $response = $response->withHeader('Access-Control-Allow-Headers', $requestHeaders);
+        $response = $response->withHeader('Access-Control-Allow-Credentials', 'true');
+
+        return $response;
+    }
+}
