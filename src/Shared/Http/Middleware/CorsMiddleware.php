eb627a4474a2aba05ccfbe94a3fed7c382312ad3
dominx99 <dominx992@gmail.com>
2021-05-15 13:41:03 +0200

    Fix issues with origin

--- src/Shared/Http/Middleware/CorsMiddleware.php
+++ src/Shared/Http/Middleware/CorsMiddleware.php
@@ -6,10 +6,15 @@ use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Log\LoggerInterface;
 use Slim\Routing\RouteContext;
 
 final class CorsMiddleware implements MiddlewareInterface
 {
+    public function __construct (
+        private LoggerInterface $logger,
+    ) {}
+
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         $routeContext = RouteContext::fromRequest($request);
@@ -17,9 +22,26 @@ final class CorsMiddleware implements MiddlewareInterface
         $methods = $routingResults->getAllowedMethods();
         $requestHeaders = $request->getHeaderLine('Access-Control-Request-Headers');
 
+        $origin = $request->getHeader('Origin');
+
+        if (is_array($origin) && count($origin) > 0) {
+            $origin = $origin[0];
+        }
+
+        if (! $origin) {
+            $origin = env('FRONT_BASE_URL');
+        }
+
+        $allowedOrigins = explode(',', env('ALLOWED_ORIGINS'));
+
         $response = $handler->handle($request);
 
-        $response = $response->withHeader('Access-Control-Allow-Origin', env('FRONT_BASE_URL'));
+        if (in_array($origin, $allowedOrigins)) {
+            $response = $response->withHeader('Access-Control-Allow-Origin', $origin);
+        } else {
+            $response = $response->withHeader('Access-Control-Allow-Origin', env('FRONT_BASE_URL'));
+        }
+
         $response = $response->withHeader('Access-Control-Allow-Methods', implode(',', $methods));
         $response = $response->withHeader('Access-Control-Allow-Headers', $requestHeaders);
         $response = $response->withHeader('Access-Control-Allow-Credentials', 'true');
